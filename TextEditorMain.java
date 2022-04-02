1448f7bee91ce49b03dc60c8ea1999f35ec188dc
David <87758475+Redseason1@users.noreply.github.com>
2022-04-03 02:19:44 +0800

    Update TextEditorMain.java

--- TextEditorMain.java
+++ TextEditorMain.java
@@ -2,6 +2,6 @@ package TextEditor;
 
 public class TextEditorMain {
     public static void main(String args[]){
-        new TextEditor();  //尚有bug: 關掉後再load原txt檔沒辦法回復上一次的儲存
+        new TextEditor();
     }
 }
